685cb640aed1ed32ee8071f0968b865a5a22bc19
William Hutton <williamhutton@linuxmail.org>
2026-05-26 12:19:11 +0100

    Update optimize-boot.sh

--- scripts/optimize-boot.sh
+++ scripts/optimize-boot.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+# ========================================================================
+# Lubuntu System Tools - Boot Optimizer
+# Copyright (C) 2026 William Hutton
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License.
+# ========================================================================
 
 # Ensure the script is run with sudo
 if [ "$EUID" -ne 0 ]; then
