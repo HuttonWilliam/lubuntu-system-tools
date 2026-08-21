38c8b9658fdd2507a650455b04634b0c2f2bb356
William Hutton <williamhutton@linuxmail.org>
2026-08-21 07:29:46 +0100

    Update battery-monitor.sh

--- scripts/battery-monitor.sh
+++ scripts/battery-monitor.sh
@@ -3,8 +3,7 @@
 # Lubuntu System Tools - battery-monitor
 # Copyright (C) 2026 William Hutton
 # 
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License.
 # ========================================================================
 
